8e4ab9bfbb124cb95ca10e4892ccb0d65329ccba
dragonshade <powerfuldarkness@hotmail.com>
2014-01-27 14:32:52 -0800

    fixed destroy method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,9 +20,9 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @posts=Post.allsortbydate
     @post = Post.find(params[:id])
     @post.destroy
+    @posts=Post.allsortbydate
   end
 
   private
